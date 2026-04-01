34f1c18083e40892e3870c67b664361f81830dc5
michalantolik <michal.antolik@gmail.com>
2026-04-01 03:09:35 +0200

    Install Django apps in Django project

--- AIPoweredPythonBlog/ai_powered_blog/ai_powered_blog/settings.py
+++ AIPoweredPythonBlog/ai_powered_blog/ai_powered_blog/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'comments',
+    'core',
+    'posts',
+    'tags',
+    'users',
     'website'
 ]
 
